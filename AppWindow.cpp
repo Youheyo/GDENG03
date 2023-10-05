f2867a5e598fd8853882ca38a5166f793e0f3937
Youheyo <80012381+Youheyo@users.noreply.github.com>
2023-10-05 19:46:52 +0800

    Matrix Implementation
    Cube won't move. Need to find out why

--- AppWindow.cpp
+++ AppWindow.cpp
@@ -1,22 +1,20 @@
 #include "AppWindow.h"
 #include <Windows.h>
-
-struct vec3 {
-	float x, y, z;
-};
+#include <iostream>
 
 struct vertex {
-	// vec3 startposition;
-	// vec3 endposition;
-	vec3 color;
-	vec3 color1;
+	Vector3D color;
+	Vector3D color1;
 
-	vec3 position;
-	vec3 position1;
+	Vector3D position;
+	Vector3D position1;
 };
 
 __declspec(align(16))
 struct constant{
+	Matrix4x4 m_world;
+	Matrix4x4 m_view;
+	Matrix4x4 m_proj;
 	unsigned int m_time;
 };
 
@@ -27,6 +25,39 @@ AppWindow::AppWindow()
 
 AppWindow::~AppWindow()
 {
+}
+
+void AppWindow::updateQuadPosition()
+{
+	constant cc;
+	cc.m_time = ::GetTickCount();
+
+	m_delta_pos += m_delta_time / 10.0f;
+	if (m_delta_pos > 1.0f) {
+		m_delta_pos = 0;
+	}
+	Matrix4x4 temp;
+
+	//cc.m_world.setTranslation(Vector3D::lerp(Vector3D(-2,-2,0), Vector3D(2,2,0), m_delta_pos));
+	
+	m_delta_scale += m_delta_time / 0.15f;
+
+	cc.m_world.setScale(Vector3D::lerp(Vector3D(0.5f, 0.5f, 0), Vector3D(1.0f,1.0f,0), (sin(m_delta_scale) + 1.0f)/ 2.0f));
+
+	temp.setTranslation(Vector3D::lerp(Vector3D(-1.5f,-1.5f,0), Vector3D(1.5f,1.5f,0), m_delta_pos));
+
+	cc.m_world *= temp;
+
+	cc.m_view.setIdentity();
+	cc.m_proj.setOrthoLH(
+		(this->getClientWindowRect().right - this->getClientWindowRect().left) / 400.0f,
+		(this->getClientWindowRect().bottom - this->getClientWindowRect().top) / 400.0f,
+		-4.0f,
+		4.0f
+	);
+
+	m_cb->update(GraphicsEngine::get()->getImmediateDeviceContext(), &cc);
+
 }
 
 void AppWindow::onCreate()
@@ -40,10 +71,10 @@ void AppWindow::onCreate()
 
 	vertex list[] = {
 	//POS X - Y - Z			POS1 X - Y - Z			Color	Color1
-		{-0.5f,-0.5f,0.0f,	-0.32f, -0.11f, 0.0f, 	1,0,0,	0,1,0},
-		{-0.5f,0.5f,0.0f,	-0.11f, 0.78f, 0.0f, 	0,1,0,	1,1,0},
-		{ 0.5f,-0.5f,0.0f,	0.75f, -0.73f, 0.0f, 	0,0,1,	1,0,0},
-		{ 0.5f,0.5f,0.0f,	0.88f, 0.77f, 0.0f, 	1,1,0,	0,0,1}
+		{Vector3D(-0.5f,-0.5f,0.0f),	Vector3D(-0.32f, -0.11f, 0.0f),	Vector3D(1,0,0), Vector3D(0,1,0)},
+		{Vector3D(-0.5f,0.5f,0.0f),		Vector3D(-0.11f, 0.78f, 0.0f), 	Vector3D( 0,1,0),Vector3D(1,1,0)},
+		{Vector3D( 0.5f,-0.5f,0.0f),	Vector3D(0.75f, -0.73f, 0.0f), 	Vector3D( 0,0,1),Vector3D(1,0,0)},
+		{Vector3D( 0.5f,0.5f,0.0f), 	Vector3D(0.88f, 0.77f, 0.0f), 	Vector3D( 1,1,0), Vector3D(0,0,1)}
 	};
 
 	m_vb = GraphicsEngine::get()->createVertexBuffer();
@@ -86,10 +117,9 @@ void AppWindow::onUpdate()
 	RECT rc = this->getClientWindowRect();
 	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
 
-	constant cc;
 
-	cc.m_time = ::GetTickCount();
-	m_cb->update(GraphicsEngine::get()->getImmediateDeviceContext(), &cc);
+	updateQuadPosition();
+
 	GraphicsEngine::get()->getImmediateDeviceContext()->setConstantBuffer(m_vs, m_cb);
 	GraphicsEngine::get()->getImmediateDeviceContext()->setConstantBuffer(m_ps, m_cb);
 
@@ -105,6 +135,11 @@ void AppWindow::onUpdate()
 	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleStrip(m_vb->getSizeVertexList(),0);
 
 	m_swap_chain->present(false);
+
+	m_old_delta = m_new_delta;
+	m_new_delta = ::GetTickCount();
+
+	m_delta_time = (m_old_delta)?((m_new_delta - m_old_delta) / 1000.0f):0;
 
 }
 
--- AppWindow.h
+++ AppWindow.h
@@ -7,13 +7,16 @@
 #include "VertexShader.h"
 #include "PixelShader.h"
 #include "ConstantBuffer.h"
-
+#include "Vector3D.h"
+#include "Matrix4x4.h"
 
 class AppWindow : public Window
 {
 public:
 	AppWindow();
 	~AppWindow();
+
+	void updateQuadPosition();
 
 	// Inherited via Window
 	virtual void onCreate() override;
@@ -26,4 +29,12 @@ private:
 	VertexShader* m_vs;
 	PixelShader* m_ps;
 	ConstantBuffer* m_cb;
+
+	float m_old_delta;
+	float m_new_delta;
+	float m_delta_time;
+
+	float m_delta_pos;
+	float m_delta_scale;
+
 };
